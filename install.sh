fb97d0c33bad22b07065a3f27003513e2ac38cdd
tknr <geishagirlskoume@gmail.com>
2025-02-24 23:01:20 +0900

    2025年  2月 24日 月曜日 23:01:20 JST

--- install.sh
+++ install.sh
@@ -3,16 +3,17 @@ export IFS=$'\n'
 cd `dirname $0`
 
 INSTALL_LIST=(
+"dl_abema"
 "dl_aria2c"
 "dl_bc"
 "dl_curl"
 "dl_freyr"
+"dl_github_release"
 "dl_mega"
 "dl_sc"
 "dl_streamlink"
 "dl_uptobox"
 "dl_youtube"
-"dl_github_release"
 "dl_ytmdl"
 )
 
